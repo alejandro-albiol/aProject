f1b953a9d26f31c8b8fdb495f273e08b105adf3b
Alejandro Albiol <albiolsafontalejandro@gmail.com>
2024-10-16 13:24:10 +0200

    feat: serve static files

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from 'url';
 
 const app = express();
 const port = 3000;
 
+// Convert import.meta.url to __dirname equivalent
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const publicPath = path.join(__dirname, '../public');//Indica la ruta desde la que se va a servir el contenido estático, desde el directorio actual (src) hasta el directorio public
+
+app.use(express.static(publicPath));//Sirve contenido estático desde el directorio public
+
 app.get('/', (req: express.Request, res: express.Response) => {
-  res.send('<h1>Hello, World!</h1>');
+  res.sendFile(path.join(publicPath, 'index.html'));//Sirve el archivo index.html desde el directorio public
+});
+
+app.get('/page2', (req: express.Request, res: express.Response) => {
+  res.sendFile(path.join(publicPath, 'index2.html'));//Sirve el archivo index2.html desde el directorio public
 });
 
 app.listen(port, () => {
